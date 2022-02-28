566814703e79680bea81338e4c79028c1b19b159
bishal <bishalstha766@gmail.com>
2022-02-28 21:27:02 +0545

    adding all files

---
+++ blog/models.py
@@ -0,0 +1,22 @@
+from email.policy import default
+from turtle import title
+from django.db import models
+from django.utils import timezone
+from django.urls import reverse
+from django.contrib.auth.models import User
+
+# Create your models here.
+
+class Post(models.Model):
+    title = models.CharField(max_length=20)
+    content = models.TextField()
+    date_posted = models.DateTimeField(default=timezone.now)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
+
+    def get_absolute_url(self):
+        return reverse('post-detail', kwargs={'pk': self.pk})
+
+    
